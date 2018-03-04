bddee648a29256d8b82eca78edd803b2d5780cb7
sunlis <921034403@qq.com>
2018-03-04 20:27:32 +0800

    Update ms.py

--- ms.py
+++ ms.py
@@ -3,27 +3,11 @@ import pymysql
 import calendar
 
 mysql_option = {
-    "host": "119.23.153.216",
+    "host": "127.0.0.1",
     "port": 3306,
-    "user": "root",
+    "user": "sls",
     "database":"django_aip",
-    "password": "gxtangyu",
-    'cursorclass': pymysql.cursors.DictCursor,
-    'charset': "utf8",
-    "local_infile": True
-}
-conn = pymysql.connect(**mysql_option)
-cursor = conn.cursor()
-
-
-
-
-mysql_option = {
-    "host": "119.23.153.216",
-    "port": 3306,
-    "user": "root",
-    "database":"django_aip",
-    "password": "gxtangyu",
+    "password": "ggsmd",
     'cursorclass': pymysql.cursors.DictCursor,
     'charset': "utf8",
     "local_infile": True
