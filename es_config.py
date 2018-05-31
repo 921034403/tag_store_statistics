28ebd89b14305b3470ab3b9afc6ea0e2a825bff3
逍遥子 <921034403@qq.com>
2018-05-31 10:28:59 +0800

    服务同步

--- es_config.py
+++ es_config.py
@@ -1,12 +1,14 @@
 
 from elasticsearch import Elasticsearch,helpers
-from elasticsearch_dsl import Search
+from ms import cursor,conn
 
+
+cursor.execute("select max(id) as id from third_part_wechat_mall")
+res = cursor.fetchone()
+max_mall =res.get("id")
 es_mall_lis = [21,18,10,11,12,13,14,15,16,20,22,23,25,26,27,28,29,30,31,32,33]
-es_mall_lis = es_mall_lis+[i for i in range(64,155)]
-# es_mall_lis = [25]
+es_mall_lis = es_mall_lis+[i for i in range(64,max_mall)]
 
 host = "119.29.223.117"
 
 client = Elasticsearch(hosts=host)
-praise_es = 'django_aip_es_praise_v6'
